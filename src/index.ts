b2c5ca163fc1bedde0a2cfcebf0c21d296d73b06
Sean Sube <seansube@gmail.com>
2018-12-04 20:19:59 -0600

    export log with level helper

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export { ContainerNotBoundError } from 'src/error/ContainerNotBoundError';
 export { MissingValueError } from 'src/error/MissingValueError';
 export { getInject, Inject } from 'src/Inject';
 export { ConsoleLogger } from 'src/logger/ConsoleLogger';
-export { Logger } from 'src/logger/Logger';
+export { Logger, logWithLevel } from 'src/logger/Logger';
 export { NullLogger } from 'src/logger/NullLogger';
 export { Module } from 'src/Module';
 export { getProvides, Provides } from 'src/Provides';
