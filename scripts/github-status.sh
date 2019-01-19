93aeafbe1b6fa11780b3e636a675357cf89291c6
Sean Sube <seansube@gmail.com>
2019-01-19 17:14:14 -0600

    build: add status and coverage jobs

---
+++ scripts/github-status.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+STATUS="${1}"
+CI_COMMIT_SHA="${CI_COMMIT_SHA:-$(git rev-parse HEAD)}"
+
+STATUS_BODY="{\"state\": \"${STATUS}\", \"target_url\": \"${CI_PIPELINE_URL}\", \"description\": \"CI pipeline ${STATUS}!\", \"context\": \"continuous-integration/gitlab\"}"
+
+printf "Reporting status for %s...\n%s" "${CI_COMMIT_SHA}" "${STATUS_BODY}"
+printf "%s" "${STATUS_BODY}" | curl -d @- \
+  -H "Authorization: token $(printf "%s" "${GITHUB_SECRET}" | base64 -d)" \
+  -i "https://api.github.com/repos/ssube/noicejs/statuses/${CI_COMMIT_SHA}"
