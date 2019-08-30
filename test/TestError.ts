af636c3ab269299409087d0d4045aa1e5ad5b931
ssube <seansube@gmail.com>
2019-08-30 00:40:49 -0500

    feat(test): inner error via cause

--- test/TestError.ts
+++ test/TestError.ts
@@ -32,6 +32,13 @@ describe('errors', () => {
         const err = new errorType('outer error', inner);
         expect(err.stack).to.include('inner', 'inner error message').and.include('outer', 'outer error message');
       });
+
+      it('should have the nested error', () => {
+        const inner = new Error('inner error');
+        const err = new errorType('outer error', inner);
+        expect(err.cause()).to.equal(inner);
+        expect(err.length).to.equal(1);
+      });
     });
   }
 });
