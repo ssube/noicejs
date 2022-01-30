b4b13c808b81e988b3da419da87ce7c731f050f3
Sean Sube <seansube@gmail.com>
2022-01-29 19:41:04 -0600

    fix(build): correct GPG socket in dev container

--- config/image.mk
+++ config/image.mk
@@ -1,12 +1,10 @@
 image-run: ## run the development image
 image-run:
-	podman run -it \
-		-e HOME=${HOME} \
+	podman run --rm -it \
 		-e SSH_AUTH_SOCK=${SSH_AUTH_SOCK} \
 		-v /run/user/$(shell id -u)/:/run/user/$(shell id -u)/:ro \
 		-v $(shell dirname ${SSH_AUTH_SOCK}):$(shell dirname ${SSH_AUTH_SOCK}):rw \
-		-v ${HOME}/.gnupg/:/.gnupg/:ro \
+		-v ${HOME}/.gnupg/:/root/.gnupg/:rw \
 		-v $(ROOT_PATH):$(ROOT_PATH):rw \
 		-w $(ROOT_PATH) \
 		docker.artifacts.apextoaster.com/apextoaster/node:16.6 bash
-#	-u $(shell id -u):$(shell id -g) \
