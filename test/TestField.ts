4f3b21461db76f5f477730f91d5cfd0208d2539a
Sean Sube <seansube@gmail.com>
2022-10-18 22:42:57 -0500

    fix(test): cover basic usage of field decorator

--- test/TestField.ts
+++ test/TestField.ts
@@ -20,9 +20,6 @@ describe('field decorator', async () => {
 
         fillFields(this, props as any);
       }
-
-      /* c8 ignore next */
-      private method() { /* noop */ }
     }
 
     const target = new Target({
@@ -31,5 +28,127 @@ describe('field decorator', async () => {
 
     expect(getFields(target).length).to.equal(1);
     expect(target.foo).to.equal(4);
+  });
+
+  it('should work on multiple fields', async () => {
+    const numberSymbol = Symbol('number');
+    const stringSymbol = Symbol('string');
+
+    interface TargetProps {
+      [numberSymbol]: number;
+      [stringSymbol]: string;
+    }
+
+    class Target {
+      @Field(numberSymbol)
+      public foo: number;
+
+      @Field(stringSymbol)
+      public bar: string;
+
+      constructor(props: TargetProps) {
+        this.foo = 0;
+        this.bar = '';
+
+        fillFields(this, props);
+      }
+    }
+
+    const target = new Target({
+      [numberSymbol]: 4,
+      [stringSymbol]: 'bar',
+    });
+
+    expect(getFields(target).length).to.equal(2);
+    expect(target.bar).to.equal('bar');
+  });
+
+  it('should handle duplicate fields', async () => {
+    const numberSymbol = Symbol('number');
+
+    interface TargetProps {
+      [numberSymbol]: number;
+    }
+
+    class Target {
+      @Field(numberSymbol)
+      public bar: number;
+
+      @Field(numberSymbol)
+      public foo: number;
+
+      constructor(props: TargetProps) {
+        this.bar = 0;
+        this.foo = 0;
+
+        fillFields(this, props);
+      }
+    }
+
+    const target = new Target({
+      [numberSymbol]: 4,
+    });
+
+    expect(target.bar).to.equal(4);
+    expect(target.foo).to.equal(4);
+  });
+
+  it('should handle duplicate decorators', async () => {
+    const numberSymbol = Symbol('number');
+    const stringSymbol = Symbol('string');
+
+    interface TargetProps {
+      [numberSymbol]: number;
+    }
+
+    class Target {
+      @Field(numberSymbol)
+      @Field(stringSymbol)
+      public bar: number;
+
+      constructor(props: TargetProps) {
+        this.bar = 0;
+
+        fillFields(this, props);
+      }
+    }
+
+    const target = new Target({
+      [numberSymbol]: 4,
+    });
+
+    expect(target.bar).to.equal(4);
+  });
+
+  it('should not work on classes', async () => {
+    const numberSymbol = Symbol('number');
+
+    expect(() => {
+      @Field(numberSymbol)
+      class Target {
+        // empty
+      }
+    }).to.throw('field decorator must be used on a field');
+  });
+
+  it('should handle missing values', async () => {
+    const numberSymbol = Symbol('number');
+
+    interface TargetProps {
+      [numberSymbol]: number;
+    }
+
+    class Target {
+      @Field(numberSymbol)
+      public bar: number;
+
+      constructor(props: Partial<TargetProps>) {
+        this.bar = 0;
+
+        fillFields(this, props);
+      }
+    }
+
+    expect(() => new Target({})).to.throw('missing value for field');
   });
 });
