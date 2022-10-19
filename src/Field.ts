2f61059629b3b68b3c6456daa4eb5d0d7d0b156c
Sean Sube <seansube@gmail.com>
2022-10-18 22:13:00 -0500

    feat(field): add helper to populate fields

--- src/Field.ts
+++ src/Field.ts
@@ -1,7 +1,12 @@
-import { ContractName } from './Container.js';
-import { Dependency } from './Dependency.js';
+import { AnyContract, ContractName } from './Container.js';
 import { InvalidTargetError } from './error/InvalidTargetError.js';
-import { doesExist, isNil, resolveDescriptor } from './utils/index.js';
+import { MissingValueError } from './error/MissingValueError.js';
+import { doesExist, isNil } from './utils/index.js';
+
+export interface Binding {
+  key: string;
+  name: ContractName;
+}
 
 export const fieldSymbol = Symbol('noicejs-field');
 
@@ -13,20 +18,23 @@ export const fieldSymbol = Symbol('noicejs-field');
  * @public
  */
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-export function getFields(target: any): Array<Dependency> {
+export function getFields(target: any): Array<Binding> {
   if (Reflect.has(target, fieldSymbol)) {
     const existing = Reflect.get(target, fieldSymbol);
+    console.log('target has fields', target, existing);
     if (Array.isArray(existing)) {
       return existing;
     }
   } else {
     // first dep for this target, check prototype
     const proto = Reflect.getPrototypeOf(target);
+    console.log('target does not have fields, checking prototype', target, proto);
     if (doesExist(proto) && proto !== target) {
       return getFields(proto);
     }
   }
 
+  console.log('making new fields for target', target);
   return [];
 }
 
@@ -39,29 +47,38 @@ export function getFields(target: any): Array<Dependency> {
  */
 export function Field(gets: ContractName) {
   /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  return (target: any, key?: string, providedDesc?: PropertyDescriptor) => {
+  return (target: any, key?: string, _providedDesc?: PropertyDescriptor) => {
     if (isNil(key)) {
       throw new InvalidTargetError('field decorator must be used on a field');
     } else {
-      // is this worth checking?
-      const desc = resolveDescriptor(target, key, providedDesc);
-      if (typeof desc.value === 'function') {
-        throw new InvalidTargetError('field decorator cannot inject methods');
-      }
-
       const fields = getFields(target);
       const prev = fields.find((it) => it.name === key);
 
+      console.log('binding field', target, key, gets);
       if (doesExist(prev)) {
-        prev.contract = gets;
+        prev.name = gets;
       } else {
         fields.push({
-          contract: gets,
-          name: key,
+          key,
+          name: gets,
         });
       }
 
-      Reflect.set(desc.value, fieldSymbol, prev);
+      Reflect.set(target, fieldSymbol, fields);
     }
   };
+}
+
+export function fillFields(target: object, values: Record<ContractName, AnyContract>): void {
+  const proto = Reflect.getPrototypeOf(target);
+  const fields = getFields(proto);
+
+  console.log('filling fields on target', fields.length, fields, target);
+  for (const field of fields) {
+    if (Reflect.has(values, field.name)) {
+      Reflect.set(target, field.key, values[field.name]);
+    } else {
+      throw new MissingValueError('missing value for field');
+    }
+  }
 }
